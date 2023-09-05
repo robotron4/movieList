a72989f605246f6e31f50be8298c1c4f14895615
robotron4 <vornholt.leon@gmail.com>
2023-09-05 15:30:11 +0200

    add useRef

--- src/App.jsx
+++ src/App.jsx
@@ -14,11 +14,14 @@ import SelectedMovie from "./main/SelectedMovie";
 
 function App() {
   const [movies, setMovies] = useState([]);
-  const [watched, setWatched] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
   const [query, setQuery] = useState("");
   const [selectedId, setSelectedId] = useState(null);
+  const [watched, setWatched] = useState(() => {
+    const storedValue = localStorage.getItem("watched");
+    return JSON.parse(storedValue);
+  });
 
   function handleSelectedId(id) {
     setSelectedId((selectedId) => (selectedId === id ? null : id));
@@ -34,6 +37,13 @@ function App() {
   function handleDeleteWatched(id) {
     setWatched((watched) => watched.filter((movie) => movie.imdbID !== id));
   }
+
+  useEffect(
+    function () {
+      localStorage.setItem("watched", JSON.stringify(watched));
+    },
+    [watched]
+  );
 
   useEffect(
     function () {
--- src/nav/Search.jsx
+++ src/nav/Search.jsx
@@ -1,5 +1,11 @@
 /* eslint-disable react/prop-types */
+import { useRef, useEffect } from "react";
 export default function Search({ query, setQuery }) {
+  const inputEl = useRef(null);
+
+  useEffect(function () {
+    inputEl.current.focus();
+  });
   return (
     <input
       className="search"
@@ -7,6 +13,7 @@ export default function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputEl}
     />
   );
 }
